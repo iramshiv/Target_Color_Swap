b486720498f772c6e44b49cff893b07024121e85
iRamShiv <iramshiv@gmail.com>
2022-04-26 11:33:40 +0200

    Add files via upload

---
+++ kmeans.py
@@ -0,0 +1,153 @@
+import os
+
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import cv2
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import MinMaxScaler
+from os import path
+from matplotlib.pyplot import ion
+from sklearn.cluster import KMeans
+from joblib import Parallel, delayed
+
+__author__ = "Sethuraman"
+__email__ = "sethuraman@inato.co.in"
+
+def kmeanss(simage, min_k, max_k):
+    src_image = cv2.imread(simage)
+    image = cv2.cvtColor(src_image, cv2.COLOR_BGR2RGB)
+    # plt.figure()
+    # plt.axis("off")
+    # plt.imshow(image)
+
+    # reshape the image to be a list of pixels
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+    # cluster the pixel intensities
+    k_range = range(min_k, max_k)
+    print("------Initiating Clustering!!!")
+    best_k = choose_best_k_parallel(simage, k_range)
+    # print(best_k)
+    clt = KMeans(n_clusters=best_k)
+    clt.fit(image)
+    print("------Clustering Finished!!!")
+
+    # build a histogram of clusters and then create a figure
+    # representing the number of pixels labeled to each color
+    hist = centroid_histogram(clt)
+    bar = plot_colors(hist, clt.cluster_centers_)
+
+    # show our color bar
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(bar)
+    ion()
+    plt.show()
+
+    """ --- Calculating cluster data min and max ----
+
+    target_cluster = input('Enter the target cluster:')
+    target_cluster = int(target_cluster)
+
+    a = []
+    b = []
+    c = []
+
+    for i in image[clt.labels_ == target_cluster]:
+        a.append(i[0])
+        b.append(i[1])
+        c.append(i[2])
+
+    return clt.cluster_centers_, a, b, c
+    
+    """
+    return clt.cluster_centers_
+
+
+def centroid_histogram(clt):
+    # grab the number of different clusters and create a histogram
+    # based on the number of pixels assigned to each cluster
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+    # normalize the histogram, such that it sums to one
+    hist = hist.astype("float")
+    hist /= hist.sum()
+    # return the histogram
+    return hist
+
+
+def plot_colors(hist, centroids):
+    # initialize the bar chart representing the relative frequency
+    # of each of the colors
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+    # loop over the percentage of each cluster and the color of
+    # each cluster
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # return the bar chart
+    return bar
+
+
+def choose_best_k_parallel(data, k_range):
+    """
+    Returns
+    -------
+    best_k: int
+        chosen value of k out of the given k range.
+        chosen k is k with the minimum inertia value.
+    results: (int) best k
+    """
+    data = img.imread(data)
+    r = []
+    g = []
+    b = []
+
+    for row in data:
+        for pixel in row:
+            # A pixel contains RGB values
+            r.append(pixel[0])
+            g.append(pixel[1])
+            b.append(pixel[2])
+
+    df = pd.DataFrame({'red': r, 'green': g, 'blue': b})
+
+    # choose features
+    data_for_clustering = df.copy()
+
+    # create data matrix
+    data_matrix = np.asarray(data_for_clustering).astype(float)
+
+    # scale the data
+    mms = MinMaxScaler()
+    data = mms.fit_transform(data_matrix)
+
+    ans = Parallel(n_jobs=-1, verbose=10)(delayed(kmeansres)(data, k) for k in k_range)
+    ans = list(zip(k_range, ans))
+    results = pd.DataFrame(ans, columns=['k', 'Scaled Inertia']).set_index('k')
+    best_k = results.idxmin()[0]
+    return best_k
+
+
+def kmeansres(data, k, alpha_k=0.02):
+    """
+    alpha_k: float
+        manually tuned factor that gives penalty to the number of clusters
+    Returns
+    -------
+    scaled_inertia: float
+        scaled inertia value for current k
+    """
+
+    inertia_o = np.square((data - data.mean(axis=0))).sum()
+    # fit k-means
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(data)
+    inertia = kmeans.inertia_ / inertia_o + alpha_k * k
+    return inertia
---
+++ testswap.py
@@ -0,0 +1,91 @@
+from PIL import Image
+import argparse
+
+__author__ = "Sethuraman"
+__email__ = "sethuraman@inato.co.in"
+
+#  construct the argument parser and parse the arguments
+from kmeans.kmeans import kmeanss
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True)
+ap.add_argument("-o", "--output", required=True)
+ap.add_argument("-rgb", "--target_rgb", required=True, nargs='+', type=int, default=[0, 0, 0])
+ap.add_argument("-k", "--mink", default=2, type=int)
+ap.add_argument("-k1", "--maxk", default=7, type=int)
+ap.add_argument("-s", "--shades_range", default=5, type=int)
+ap.add_argument("-t", "--tint_range", default=5, type=int)
+args = vars(ap.parse_args())
+
+print(args["target_rgb"][0])
+print(args["target_rgb"][1])
+print(args["target_rgb"][2])
+
+# generating centers of a cluster
+cluster_centers_ = kmeanss(args["image"], args["mink"], args["maxk"])
+
+# input target cluster
+target_cluster = input("Enter the target cluster from the plot generated (clusters starts from '0'):")
+target_cluster = int(target_cluster)
+
+src_img = Image.open(args["image"])
+# src_img.show()
+
+img_width = src_img.size[0]
+img_height = src_img.size[1]
+
+# shades ::: to find the nearest dark color range
+shade_range = args["shades_range"]
+# tint ::: to find the nearest light color range
+tint_range = args["tint_range"]
+
+# generate target colour range (RGB values)
+t = round(int(cluster_centers_[target_cluster][0])) - (shade_range * 10)
+t1 = round(int(cluster_centers_[target_cluster][0])) + (tint_range * 10)
+
+t2 = round(int(cluster_centers_[target_cluster][1])) - (shade_range * 13)
+t3 = round(int(cluster_centers_[target_cluster][1])) + (tint_range * 7)
+
+t4 = round(int(cluster_centers_[target_cluster][2])) - (shade_range * 7)
+t5 = round(int(cluster_centers_[target_cluster][2])) + (tint_range * 13)
+
+
+def roundoff(tt):
+    tt = int(tt)
+    if tt > 255:
+        tt = 255
+    elif tt < 0:
+        tt = 0
+    else:
+        tt = tt
+    return tt
+    print(tt)
+
+
+"""
+t = min(a) - 15
+t1 = max(a) + 15
+
+t2 = min(b) - 6
+t3 = max(b) + 6
+
+t4 = min(c) - 6
+t5 = max(c) + 6
+print(t,t1,t2,t3,t4,t5)
+
+"""
+
+# process all pixels
+for x in range(0, img_width):
+    for y in range(0, img_height):
+        data = src_img.getpixel((x, y))
+        # logic for target and result colors swap with texture formula
+        if data[0] in range(roundoff(t), roundoff(t1)) and data[1] in range(roundoff(t2), roundoff(t3)) and data[
+            2] in range(roundoff(t4), roundoff(t5)):
+            src_img.putpixel((x, y), (
+                int((args["target_rgb"][0] / (100 / ((data[0] / t1) * 100)))), int((args["target_rgb"][1] / (100 / ((data[1] / t3) * 100)))),
+                int((args["target_rgb"][2] / (100 / ((data[2] / t5) * 100))))))
+
+src_img.save(str(args["output"]) + '/result.jpg')
+res_img = Image.open(str(args["output"]) + '/result.jpg')
+res_img.show()
